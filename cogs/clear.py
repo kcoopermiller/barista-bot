85c10a88da7361eaa2f637c89111feb6a856fade
Cooper <kcoopermiller9@gmail.com>
2021-07-20 16:48:05 -0500

    removed permissions func

--- cogs/clear.py
+++ cogs/clear.py
@@ -13,12 +13,6 @@ class Clear(commands.Cog):
         amount += 1
         await ctx.channel.purge(limit=amount)
 
-    # doesn't work
-    @clear.error
-    async def clear_error(error, ctx):
-        if isinstance(error, MissingPermissions):
-            await ctx.send("You don't have permission to do that!")
-
 
 def setup(bot):
     bot.add_cog(Clear(bot))
