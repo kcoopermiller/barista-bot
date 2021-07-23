6ed2c0d524b14243f9692d56da5d183e7a2588c9
Cooper <kcoopermiller9@gmail.com>
2021-07-23 17:18:10 -0500

    clear command restricted to administrators

--- cogs/clear.py
+++ cogs/clear.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import time
 
 
 class Clear(commands.Cog):
@@ -8,10 +9,13 @@ class Clear(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    # @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount=10):
-        amount += 1
-        await ctx.channel.purge(limit=amount)
+        if ctx.message.author.guild_permissions.administrator:
+            await ctx.channel.purge(limit=amount+1)
+        else:
+            ctx.send("Cannot clear due to insufficient permissions")
+            time.sleep(2)
+            await ctx.channel.purge(limit=1)
 
 
 def setup(bot):
