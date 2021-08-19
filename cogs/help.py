a0ab8e4bd7a1e47f35be7c1fa82c4d9fae5b6204
Cooper <kcoopermiller9@gmail.com>
2021-08-19 00:26:53 -0500

    added command explanations

--- cogs/help.py
+++ cogs/help.py
@@ -9,24 +9,7 @@ class Help(commands.Cog):
 
     @commands.command()
     async def help(self, ctx, command=None):
-        if command is not None:
-            if command == 'fact':
-                embed = discord.Embed(title='**Fact Command**', description="Fact", color=0x36393f)
-                await ctx.send(embed=embed)
-            elif command == 'help':
-                embed = discord.Embed(title='**Help Command**', description="bruh", color=0x36393f)
-                await ctx.send(embed=embed)
-            elif command =='ill':
-                embed = discord.Embed(title='**Illini Command**', description="ILL\nNI!", color=0x36393f)
-                await ctx.send(embed=embed)
-            elif command == 'pain':
-                embed = discord.Embed(title='**Pain Command**', description="pain peko", color=0x36393f)
-                await ctx.send(embed=embed)
-            elif command == 'report':
-                embed = discord.Embed(title='**Report Command**', description="report", color=0x36393f)
-                await ctx.send(embed=embed)
-        else:
-            commands = '''
+        commands = '''
 **Available Commands:**
 .fact
 .help
@@ -35,7 +18,34 @@ class Help(commands.Cog):
 .report
 
 Type `.help [command]` for more information
-            '''
+        '''
+        if command is not None:
+            if command == 'fact':
+                description = '''
+.fact 
+- display random coffee fact
+.fact [new fact] 
+- submit new fact for approval. if approved, the fact will be added to the list
+                '''
+                embed = discord.Embed(title='**Fact Command**', description=description, color=0x36393f)
+                await ctx.send(embed=embed)
+            elif command == 'help':
+                embed = discord.Embed(title='**Help Command**', description=commands, color=0x36393f)
+                await ctx.send(embed=embed)
+            elif command =='ill':
+                embed = discord.Embed(title='**Illini Command**', description=".ill\n- ni!", color=0x36393f)
+                await ctx.send(embed=embed)
+            elif command == 'pain':
+                embed = discord.Embed(title='**Pain Command**', description="pain peko", color=0x36393f)
+                await ctx.send(embed=embed)
+            elif command == 'report':
+                description = '''
+.report [channel to look at] [message explaining incident]
+- reports will be automatically deleted and sent anonymously to a mod
+                '''
+                embed = discord.Embed(title='**Report Command**', description=description, color=0x36393f)
+                await ctx.send(embed=embed)
+        else:
             embed = discord.Embed(title='**Help**', description=commands, color=0x36393f)
             await ctx.send(embed=embed)
 
