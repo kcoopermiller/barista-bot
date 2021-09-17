3c1d8258e5338a695f55268c4b9af80c0d4f971d
Cooper <kcoopermiller9@gmail.com>
2021-09-16 19:18:32 -0500

    fixed remove role by reaction

--- cogs/events.py
+++ cogs/events.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
+
 
 
 class Events(commands.Cog):
@@ -73,6 +75,71 @@ class Events(commands.Cog):
                 member = payload.member
                 if member:
                     await member.add_roles(role)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        message_id = payload.message_id
+        if message_id == 887843047268823040:
+            guild_id = payload.guild_id
+            guild = discord.utils.find(
+                lambda g: g.id == guild_id, self.bot.guilds)
+
+            role = None
+
+            if payload.emoji.name == '🟨':
+                role = discord.utils.get(guild.roles, name='freshman')
+            elif payload.emoji.name == '🟩':
+                role = discord.utils.get(guild.roles, name='sophomore')
+            elif payload.emoji.name == '🟦':
+                role = discord.utils.get(guild.roles, name='junior')
+            elif payload.emoji.name == '🟪':
+                role = discord.utils.get(guild.roles, name='senior')
+            elif payload.emoji.name == '🟥':
+                role = discord.utils.get(guild.roles, name='grad student')
+
+            if role is not None:
+                member = get(guild.members, id=payload.user_id)
+                if member:
+                    await member.remove_roles(role)
+
+        if message_id == 887843047709245470:
+            guild_id = payload.guild_id
+            guild = discord.utils.find(
+                lambda g: g.id == guild_id, self.bot.guilds)
+
+            role = None
+
+            if payload.emoji.name == '🟨':
+                role = discord.utils.get(guild.roles, name='he/him')
+            elif payload.emoji.name == '🟩':
+                role = discord.utils.get(guild.roles, name='she/her')
+            elif payload.emoji.name == '🟦':
+                role = discord.utils.get(guild.roles, name='they/them')
+            elif payload.emoji.name == '🟪':
+                role = discord.utils.get(guild.roles, name='genderfluid')
+            elif payload.emoji.name == '🟥':
+                role = discord.utils.get(guild.roles, name='multiple pronouns')
+
+            if role is not None:
+                member = get(guild.members, id=payload.user_id)
+                if member:
+                    await member.remove_roles(role)
+
+        if message_id == 887843048216748082:
+            guild_id = payload.guild_id
+            guild = discord.utils.find(
+                lambda g: g.id == guild_id, self.bot.guilds)
+
+
+            role = None
+
+            if payload.emoji.name == '☕':
+                role = discord.utils.get(guild.roles, name='promotions')
+
+            if role is not None:
+                member = get(guild.members, id=payload.user_id)
+                if member:
+                    await member.remove_roles(role)
 
 
 def setup(bot):
